be08db93ef82e72205e5f9d217ab14e0d5087025
Diego Robles <shoshincl@gmail.com>
2022-02-10 13:23:01 -0300

    fix(app):port

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,7 @@ import './env';
 import { app } from './app';
 
 if (require.main === module) {
-  app.listen(4000, () => console.log(`API URL http://localhost:4000`));
+  app.listen(process.env.PORT, () =>
+    console.log(`API URL http://localhost:4000`)
+  );
 }
